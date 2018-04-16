c6c39c47b916b664ac3ad9b99e924b496ef765a1
Robin <robinpoitevin@gmail.com>
2018-04-16 15:49:51 +0200

    Add files via upload
    Now support name from differents psd. No more half cropped psd. Bugs fixes

--- PSDS_to_PSD_ARTBOARDS.jsx
+++ PSDS_to_PSD_ARTBOARDS.jsx
@@ -3,9 +3,9 @@
 ////////////////// INFOS //////////////////
 
 // name : PSDS_TO_ARTBOARDS.jsx
-// description : Select a folder of PSD'S, and this script will create one big document with each PSD as artboard
+// description : Take all of the psd's from a folder, and create a single one, on different artboard
 // author : Robin Poitevin 
-// version : V 0.1
+// version : V 0.3
 // credits : open all files and extensions check based on functions of Jeffrey Tranberry 
 //------------------------------------------------------------------------------
 
@@ -22,22 +22,119 @@ var outputFolder = Folder.selectDialog("Select a folder for the output");
 
 ////////////////// FUNCTIONS //////////////////
 
-function createArtboard() {  // Convert files to artboard
-    var debug = "debug";
-    var dialogMode = (DialogModes.NO);
+function createArtboard() {
+  // Set
+  function step1(enabled, withDialog) {
+    if (enabled != undefined && !enabled)
+      return;
+    var dialogMode = (withDialog ? DialogModes.ALL : DialogModes.NO);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putProperty(cTID('Chnl'), sTID("selection"));
+    desc1.putReference(cTID('null'), ref1);
+    desc1.putEnumerated(cTID('T   '), cTID('Ordn'), cTID('Al  '));
+    executeAction(cTID('setd'), desc1, dialogMode);
+  };
+
+  // Make
+  function step2(enabled, withDialog) {
+    if (enabled != undefined && !enabled)
+      return;
+    var dialogMode = (withDialog ? DialogModes.ALL : DialogModes.NO);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putClass(cTID('Lyr '));
+    desc1.putReference(cTID('null'), ref1);
+    var desc2 = new ActionDescriptor();
+    desc2.putString(cTID('Nm  '), "screenBG");
+    desc1.putObject(cTID('Usng'), cTID('Lyr '), desc2);
+    desc1.putInteger(cTID('LyrI'), 9270);
+    executeAction(cTID('Mk  '), desc1, dialogMode);
+  };
+
+  // Fill
+  function step3(enabled, withDialog) {
+    if (enabled != undefined && !enabled)
+      return;
+    var dialogMode = (withDialog ? DialogModes.ALL : DialogModes.NO);
+    var desc1 = new ActionDescriptor();
+    var desc2 = new ActionDescriptor();
+    desc2.putUnitDouble(cTID('Hrzn'), cTID('#Pxl'), 345);
+    desc2.putUnitDouble(cTID('Vrtc'), cTID('#Pxl'), 446);
+    desc1.putObject(cTID('From'), cTID('Pnt '), desc2);
+    desc1.putInteger(cTID('Tlrn'), 200);
+    desc1.putBoolean(cTID('AntA'), true);
+    desc1.putEnumerated(cTID('Usng'), cTID('FlCn'), cTID('FrgC'));
+    executeAction(cTID('Fl  '), desc1, dialogMode);
+  };
+
+  // Make
+  function step4(enabled, withDialog) {
+    if (enabled != undefined && !enabled)
+      return;
+    var dialogMode = (withDialog ? DialogModes.ALL : DialogModes.NO);
     var desc1 = new ActionDescriptor();
     var ref1 = new ActionReference();
     ref1.putClass(sTID("artboardSection"));
     desc1.putReference(cTID('null'), ref1);
-        desc1.putString(cTID('Nm  '), debug);
     var ref2 = new ActionReference();
     ref2.putEnumerated(cTID('Lyr '), cTID('Ordn'), cTID('Trgt'));
     desc1.putReference(cTID('From'), ref2);
-    desc1.putInteger(sTID("layerSectionStart"), 3);
-    desc1.putInteger(sTID("layerSectionEnd"), 4);
+    desc1.putInteger(sTID("layerSectionStart"), 9271);
+    desc1.putInteger(sTID("layerSectionEnd"), 9272);
+    desc1.putString(cTID('Nm  '), "Artboard 1");
     executeAction(cTID('Mk  '), desc1, dialogMode);
-  
   };
+
+  // Select
+  function step5(enabled, withDialog) {
+    if (enabled != undefined && !enabled)
+      return;
+    var dialogMode = (withDialog ? DialogModes.ALL : DialogModes.NO);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putName(cTID('Lyr '), "screenBG");
+    desc1.putReference(cTID('null'), ref1);
+    desc1.putBoolean(cTID('MkVs'), false);
+    var list1 = new ActionList();
+    list1.putInteger(9270);
+    desc1.putList(cTID('LyrI'), list1);
+    executeAction(cTID('slct'), desc1, dialogMode);
+  };
+
+  // Delete
+  function step6(enabled, withDialog) {
+    if (enabled != undefined && !enabled)
+      return;
+    var dialogMode = (withDialog ? DialogModes.ALL : DialogModes.NO);
+    executeAction(cTID('Dlt '), undefined, dialogMode);
+  };
+
+  // Select
+  function step7(enabled, withDialog) {
+    if (enabled != undefined && !enabled)
+      return;
+    var dialogMode = (withDialog ? DialogModes.ALL : DialogModes.NO);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putName(cTID('Lyr '), "Artboard 1");
+    desc1.putReference(cTID('null'), ref1);
+    desc1.putBoolean(cTID('MkVs'), false);
+    var list1 = new ActionList();
+    list1.putInteger(3);
+    desc1.putList(cTID('LyrI'), list1);
+    executeAction(cTID('slct'), desc1, dialogMode);
+  };
+
+  step1();      // Set
+  step2();      // Make
+  step3();      // Fill
+  step4();      // Make
+  step5();      // Select
+  step6();      // Delete
+  step7();      // Select
+};
+
 
 
   function createPsd() {
@@ -68,6 +165,11 @@ function OpenFolder() { // list all files
                         //alert(fileList[i]);
                         openFile(fileList,i);
                         createArtboard();
+                        var artname = app.activeDocument.name;
+                        var artlength = artname.length;
+                        app.activeDocument.activeLayer.name = artname.substr(0,artlength - 4 );
+
+
                         duplicateArtboard();
                         app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
                         filesOpened++;
@@ -99,7 +201,7 @@ function openFile(filelisting,numb) {
 };
 
 function savePSD(output){
-  	var psdFile = new File(output + "/" + "combined_PSDS");
+  	var psdFile = new File(output + "/" + "export_test");
   	psdSaveOptions = new PhotoshopSaveOptions();
   	psdSaveOptions.embedColorProfile = true;
   	psdSaveOptions.alphaChannels = true;  
